1a5667fb0bef9fc03f32af87a43fa6de787faa2e
admin <admin@admins-MacBook-Pro.local>
2021-07-29 13:31:43 +0530

    Created Task Model

---
+++ tasks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from model_utils.models import TimeFramedModel
+from taggit.managers import TaggableManager
+
+
+# Create your models here
+
+class Task(TimeFramedModel):
+    text = models.CharField(max_length=40)
+    tag = TaggableManager()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
