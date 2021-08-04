663e98665eb118d85b10c8478f18d9af5c828b74
SudarshanTestpress <86759435+SudarshanTestpress@users.noreply.github.com>
2021-08-04 12:51:36 +0530

    merging task list view with master
    * added task list view
    
    * added task create view
    
    Co-authored-by: admin <admin@admins-MacBook-Pro.local>

--- tasks/tests.py
+++ tasks/tests.py
@@ -4,7 +4,7 @@ from django.http import response
 from django.urls import reverse, resolve
 from tasks.models import Task, Project
 from .forms import ProjectForm, TaskForm
-from .views import ProjectList
+from .views import ProjectList, TaskListView
 
 # Create your tests here.
 
@@ -42,3 +42,25 @@ class TestProjectListView(TestCase):
     def test_urls_resolve_project_list_object(self):
         view = resolve("/projects/")
         self.assertEquals(view.func.view_class, ProjectList)
+
+
+class TestTaskListView(TestCase):
+    def setUp(self):
+        self.project1 = Project.objects.create(name="Deployment")
+
+        self.task1 = Task.objects.create(
+            text="Eat", project=self.project1, completed=True
+        )
+
+        self.task2 = Task.objects.create(
+            text="Sleep", project=self.project1, completed=False
+        )
+
+    def test_page_serve_successful(self):
+        self.url = reverse("tasks:list_task", args=[self.project1.pk])
+        self.response = self.client.get(self.url)
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_url_resolve_task_list_object(self):
+        view = resolve("/projects/1")
+        self.assertEquals(view.func.view_class, TaskListView)
