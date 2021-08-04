def3c389cf857812a0e1969614c51a43d3247e78
sud1997ar <sud1997ar@gmail.com>
2021-08-05 02:20:19 +0530

    added task update view

--- tasks/tests.py
+++ tasks/tests.py
@@ -11,6 +11,7 @@ from .views import (
     ProjectDeleteView,
     TaskCreateView,
     ProjectCreateView,
+    TaskUpdateView,
 )
 
 # Create your tests here.
@@ -197,3 +198,34 @@ class TestTaskCreateView(TestCase):
             "/projects/1/tasks/create", {"text": "I am a test task", "completed": False}
         )
         self.assertEqual(Task.objects.last().project.name, self.project1.name)
+
+
+class TestTaskUpdateView(TestCase):
+    def setUp(self):
+        self.project1 = Project.objects.create(name="Deployment")
+
+        self.task1 = Task.objects.create(
+            text="Eat", project=self.project1, completed=True
+        )
+
+        self.task2 = Task.objects.create(
+            text="Sleep", project=self.project1, completed=False
+        )
+
+        self.url = reverse(
+            "tasks:update_task",
+            kwargs={"pk": self.project1.pk, "task_pk": self.task1.pk},
+        )
+
+    def test_page_serve_successful(self):
+        self.response = self.client.get(self.url)
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_url_resolve_task_create_object(self):
+        view = resolve("projects/1/tasks/1/update")
+        self.assertEquals(view.func.view_class, TaskUpdateView)
+
+    def test_presence_of_csrf(self):
+        url = reverse("tasks:update_task", args=[self.project1.pk])
+        response = self.client.get(url)
+        self.assertContains(response, "csrfmiddlewaretoken")
