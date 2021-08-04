d37608e1a154710b814eadedfd0a463102e804e9
SudarshanTestpress <86759435+SudarshanTestpress@users.noreply.github.com>
2021-08-04 11:46:39 +0530

    added task form (#4)
    Co-authored-by: sud1997ar <sud1997ar@gmail.com>

--- tasks/forms.py
+++ tasks/forms.py
@@ -11,3 +11,21 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = "__all__"
+
+
+class TaskForm(forms.ModelForm):
+    text = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Add new task..."})
+    )
+    end = forms.DateTimeField(
+        required=False,
+        input_formats=["%Y-%m-%dT%H:%M"],
+        widget=forms.DateTimeInput(
+            attrs={"type": "datetime-local", "class": "form-control"},
+            format="%Y-%m-%dT%H:%M",
+        ),
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
