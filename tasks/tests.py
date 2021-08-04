bbf766cb3a8ad102c46563890604d3c669278055
admin <admin@admins-MacBook-Pro.local>
2021-08-04 12:13:32 +0530

    added task form

--- tasks/tests.py
+++ tasks/tests.py
@@ -3,17 +3,18 @@ from django.test import TestCase
 from django.http import response
 from django.urls import reverse, resolve
 from tasks.models import Task, Project
-from .forms import ProjectForm
+from .forms import ProjectForm, TaskForm
 
 # Create your tests here.
 
 
 class FormTest(TestCase):
-    def setUp(self):
-        self.response = self.client.get(self.url)
-
     def test_projectform_inputs(self):
         self.form_data = {"name": "to do app"}
         self.form = ProjectForm(data=self.form_data)
         self.assertTrue(self.form.is_valid())
 
+    def test_taskform_inputs(self):
+        self.form_data = {"text": "to do app"}
+        self.form = TaskForm(data=self.form_data)
+        self.assertTrue(self.form.is_valid())
