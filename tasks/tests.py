c619a98b6231ecec0f45102fd1257c7e7eee87b0
SudarshanTestpress <86759435+SudarshanTestpress@users.noreply.github.com>
2021-08-04 12:24:29 +0530

    Project list view (#20)
    * added project form
    
    * added project list view
    
    Co-authored-by: admin <admin@admins-MacBook-Pro.local>

--- tasks/tests.py
+++ tasks/tests.py
@@ -4,6 +4,7 @@ from django.http import response
 from django.urls import reverse, resolve
 from tasks.models import Task, Project
 from .forms import ProjectForm, TaskForm
+from .views import ProjectList
 
 # Create your tests here.
 
@@ -18,3 +19,26 @@ class FormTest(TestCase):
         self.form_data = {"text": "to do app"}
         self.form = TaskForm(data=self.form_data)
         self.assertTrue(self.form.is_valid())
+
+
+class TestProjectListView(TestCase):
+    def setUp(self):
+        self.project1 = Project.objects.create(name="Deployment")
+
+        self.task1 = Task.objects.create(
+            text="Eat", project=self.project1, completed=True
+        )
+
+        self.task2 = Task.objects.create(
+            text="Sleep", project=self.project1, completed=False
+        )
+
+        self.url = reverse("tasks:list_projects")
+
+    def test_page_serve_successful(self):
+        self.response = self.client.get(self.url)
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_urls_resolve_project_list_object(self):
+        view = resolve("/projects/")
+        self.assertEquals(view.func.view_class, ProjectList)
---
+++ tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+
+app_name = "tasks"
+
+urlpatterns = [
+    path("projects/", views.ProjectList.as_view(), name="list_projects"),
+]
